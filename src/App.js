079dd7dc715e7c99e9ea9f715a9e4094d1b4951a
AleksandrKompanetc <moneymacker7@gmail.com>
2024-06-26 20:05:30 +0300

    adding first files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MainPage from './pages/main-page/MainPage';
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
